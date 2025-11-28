ae78e2735022d9ad294e47f05e2d85d4dc08f98e
janesvami <rem.cashew@yandex.ru>
2025-11-28 19:30:13 +0200

    add TimeZone UTC to application.yml and BeautyMarathonApplication.java

--- src/main/java/ani/beautymarathon/BeautyMarathonApplication.java
+++ src/main/java/ani/beautymarathon/BeautyMarathonApplication.java
@@ -1,12 +1,20 @@
 package ani.beautymarathon;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BeautyMarathonApplication {
 
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(BeautyMarathonApplication.class, args);
     }
 
