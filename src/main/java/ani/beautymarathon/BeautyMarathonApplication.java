163e0864ae0deea562aba53adcf8c0e59bc8227c
janesvami <rem.cashew@yandex.ru>
2025-12-04 19:55:50 +0200

    fix

--- src/main/java/ani/beautymarathon/BeautyMarathonApplication.java
+++ src/main/java/ani/beautymarathon/BeautyMarathonApplication.java
@@ -1,6 +1,5 @@
 package ani.beautymarathon;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +8,6 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class BeautyMarathonApplication {
 
-    @PostConstruct
-    void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-    }
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(BeautyMarathonApplication.class, args);
