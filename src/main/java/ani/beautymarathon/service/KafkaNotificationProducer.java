a4b824df4fbe798c583049499817eee1554dba71
udodikdmitro <udodikdmitro7@gmail.com>
2026-01-11 01:33:00 +0200

    Fix the text

--- src/main/java/ani/beautymarathon/service/KafkaNotificationProducer.java
+++ src/main/java/ani/beautymarathon/service/KafkaNotificationProducer.java
@@ -6,16 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.kafka.support.SendResult;
-
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 @Service
 @Slf4j
